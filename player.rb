7987807f079efea0c3aeaf9a875dbb6d8b3b694c
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-12-11 07:24:54 -0700

    create Player class

---
+++ player.rb
@@ -0,0 +1,17 @@
+class Player
+  attr_accessor :name :score
+
+  def initialize(name)
+    @name = name
+    @score = 3
+  end
+
+  def remove_point
+    @score -= 1
+  end
+
+  def did_lose?
+    @score <= 0
+  end
+
+end 

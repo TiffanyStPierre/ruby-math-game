dd05fb9094fa06209052bbe9e8af017197f286c6
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-12-11 07:28:33 -0700

    create Question class

---
+++ question.rb
@@ -0,0 +1,18 @@
+class Question
+  attr_reader :answer
+
+  def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @answer = @num1 + @num2
+  end
+
+  def create_question
+    "What is #{@num1} + #{@num2}?"
+  end
+
+  def correct_answer?(response)
+    response.to_i == @answer
+  end
+
+end
